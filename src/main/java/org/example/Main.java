30f87f58915dcd1feee431da3d887834ba4bbbe3
Kjeff24 <jeffreyarthur123@gmail.com>
2025-03-01 21:23:21 +0000

    Tested image functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import org.example.service.ImageProcessor;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your full name: ");
+        String name = sc.nextLine();
+
+        System.out.print("\nEnter the file Path: ");
+        String filePath = sc.nextLine();
+
+        System.out.println(ImageProcessor.processImageWithWatermark(filePath, name));
 
     }
 }
